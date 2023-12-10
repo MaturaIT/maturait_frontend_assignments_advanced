f406140f5700cfd0ff6998f96274f5b56bae0ab5
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-10 17:23:41 +0100

    Created structure of the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1 class="text-3xl">MaturaIT Shop</h1>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <cart-icon />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import CartIcon from '@/icons/CartIcon.vue'
+/*
+ Include a title, link to the home page, and a basket icon.
+ Display the number of products in the cart on the basket icon.
+ Implement a dropdown on basket icon click showing cart contents and a checkout button.
+*/
 </script>
 
 <style scoped>
