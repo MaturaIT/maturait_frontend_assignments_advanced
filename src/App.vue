c27758442fc43b33cb0095ed930b49650f18fd6a
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-10 17:02:19 +0100

    Created simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1 className="text-3xl">Hello world!</h1>
+  <router-link :to="{ name: 'Home' }">Home</router-link>
   <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+</script>
+
