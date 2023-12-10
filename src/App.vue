759156bbf2827dd0d4054fec526cb85e16db87be
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-10 16:14:52 +0100

    Added vue router

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,5 @@
 
 <template>
   <h1 className="text-3xl">Hello world!</h1>
+  <router-view></router-view>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { createApp } from 'vue'
-import './index.css'
+import VueRouter from 'vue-router'
+import '@/index.css'
 
-import App from './App.vue'
+import App from '@/App.vue'
+import HomePage from '@/pages/HomePage.vue'
+
+const routes = [
+  { path: '/', component: HomePage },
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
 
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app')
