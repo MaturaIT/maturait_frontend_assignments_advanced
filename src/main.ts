10be6e39703347574f544b326db3ddae180fabf6
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-12 18:54:30 +0100

    Using plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import '@/index.css'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from '@/App.vue'
 import { router } from '@/router'
@@ -7,5 +8,6 @@ import { router } from '@/router'
 const app = createApp(App)
 
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
