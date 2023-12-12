30e68f6b07bbd7d96db49b4f11dd5276a88c1cb4
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-12 22:24:30 +0100

    Added search option

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div class="flex justify-center p-2">
+    <div class="flex flex-col items-center justify-center p-2">
       <span class="text-4xl">Home page</span>
+      <label class="p-2">Search for a product:<input class="border" type="text" v-model="search"></label>
     </div>
     <div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2 p-2">
       <div v-if="isPending" class="place-self-center"><LoadingIcon /></div>
-      <ProductItem v-else-if="isSuccess" v-for="product in data" :key="product.id" :name="product.title" :price="product.price" :image="product.image" />
+      <ProductItem v-else-if="isSuccess" v-for="product in searchedProducts" :key="product.id" :name="product.title" :price="product.price" :image="product.image" />
     </div>
   </div>
 </template>
@@ -14,6 +15,7 @@
 import ProductItem from '@/components/ProductItem.vue';
 import LoadingIcon from '@/icons/LoadingIcon.vue'
 import { useQuery } from '@tanstack/vue-query';
+import { ref, computed } from 'vue';
 
 const { isPending, isSuccess, data, error } = useQuery({
   queryKey: ['products'],
@@ -27,6 +29,11 @@ const { isPending, isSuccess, data, error } = useQuery({
   }
 })
 
+const search = ref('')
+
+const searchedProducts = computed(() => {
+  return data.value.filter((element: any) => element.title.toLowerCase().includes(search.value.toLowerCase()))
+})
 
 </script>
 
