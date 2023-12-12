74253ea04190096040fc6d70f692c446a76375fb
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-12 19:28:51 +0100

    Fixed useQuery

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,19 +3,19 @@
     Home page
     <div v-if="isPending">Is pending</div>
     <div v-else-if="isSuccess" v-for="product in data" :key="product.id">{{ product.title }}</div>
-
   </div>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query';
 
-const { isPending, isError, isSuccess, data, error } = useQuery({
+const { isPending, isSuccess, data, error } = useQuery({
   queryKey: ['products'],
   queryFn: async () => {
     const response = await fetch('https://fakestoreapi.com/products')
     if (!response.ok) {
       console.log(error)
+      return
     }
     return response.json()
   }
