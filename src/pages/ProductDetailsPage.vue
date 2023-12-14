3f583c86c135d0a751abe38e3bdd92d9ff73d61e
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-14 09:52:19 +0100

    Displaying all product data

--- src/pages/ProductDetailsPage.vue
+++ src/pages/ProductDetailsPage.vue
@@ -1,14 +1,46 @@
 <template>
-  <div>
-    {{ params.id }}
+  <div class="flex justify-center">
+    <LoadingIcon v-if="isPending" />
+    <div v-else-if="isSuccess" class="flex flex-col justify-center items-center gap-2">
+      <img :src="data.image" alt="Product image" width="256" height="256">
+      {{ data.title }}
+      {{ data.price }}
+      {{ data.category }}
+      {{ data.description }}
+      {{ data.rating }}
+    </div>
   </div>
 </template>
 
+<!-- export type Product = {
+  id: number,
+  title: string,
+  price: string,
+  category: string,
+  description: string,
+  image: string,
+  rating: number,
+} -->
+
 <script setup lang="ts">
+import LoadingIcon from '@/icons/LoadingIcon.vue';
+import { useQuery } from '@tanstack/vue-query';
 import { useRoute } from 'vue-router';
 
 
 const { params } = useRoute()
+
+const { isPending, isSuccess, data, error } = useQuery({
+  queryKey: ['product'],
+  queryFn: async () => {
+    const response = await fetch(`https://fakestoreapi.com/products/${params.id}`)
+    if (!response.ok) {
+      console.log(error)
+      return
+    }
+    return response.json()
+  }
+})
 
 </script>
 
