8ee834a49b0a700753f07640d359aa1330865cb7
Kl4y4 <surowiec.bartosz@wp.pl>
2023-12-15 14:53:49 +0100

    Added buttons to manage cart

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,12 +5,48 @@
       <h1 class="text-3xl">aturaIT Shop</h1>
     </span>
     <router-link :to="{ name: 'Home' }">Home</router-link>
-    <cart-icon />
+    <div>
+      <cart-icon @click="toggleShowCart" class="hover:bg-gray-500" />
+      <div v-if="ifShowCart" class="absolute border border-emerald-500 rounded-lg bg-slate-400">
+        <div v-for="product in cartStore.cart" :key="product.id">
+          <div>
+            <img :src="product.image" alt="Product image" width="16" height="16">
+            <span>{{ product.title }}</span>
+          </div>
+        </div>
+        <div v-if="isCartEmpty">
+          The cart is empty!
+        </div>
+        <div class="flex justify-between">
+          <button @click="clearCart" class="bg-white">Clear</button>
+          <button class="text-white bg-blue-600">Buy now</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import CartIcon from '@/icons/CartIcon.vue'
+import { useCartStore } from '@/stores/cartStore';
+import { computed, ref } from 'vue';
+
+const cartStore = useCartStore()
+
+const ifShowCart = ref(false)
+
+const toggleShowCart = () => {
+  ifShowCart.value = !ifShowCart.value
+}
+
+const clearCart = () => {
+  cartStore.clearCart()
+}
+
+const isCartEmpty = computed(() => {
+  return cartStore.cart.length === 0
+})
+
 </script>
 
 <style scoped>
