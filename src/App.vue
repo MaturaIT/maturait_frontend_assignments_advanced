4e4ed04d1f4c121febeab26993021f50b8ba0e4f
tomaszmaros <marostmsz@gmail.com>
2024-01-06 12:42:56 +0100

    Add transition to product list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <NavigationBar :sharedState="sharedState"/>
+    <NavigationBar :sharedState="sharedState" />
     <!-- Dropdown with the cart contents and checkout button, containing animation, 
         using reactive state with a boolean to get information about cart button being clicked
         from the NavigationBar component -->
     <CartItems v-if="sharedState.showCart" />
-    <ProductList />
+    <Transition name="fade">
+      <ProductList />
+    </Transition>
   </div>
 </template>
 
@@ -28,3 +30,18 @@ function incrementCartItems() {
   store.commit('incrementCartItems')
 }
 </script>
+
+<style>
+.fade-enter-active {
+  transition: all 0.2s ease-out;
+}
+
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.7, 0.9, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
